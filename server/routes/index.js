854a7bd4305676f88a3460e953eadb2ed91eabc6
Chris Nitchie <chris.nitchie@oberontech.com>
2016-12-19 14:38:53 -0500

    Fixing homepage title.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express+React Starter Project' });
+  res.render('index', { title: 'EC2 Instance Dashboard' });
 });
 
 export default router;
